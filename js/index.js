b48b842ee611fc4d3e49d26daa022459b785aae9
anoopbabu29 <anoop.bab@gmail.com>
2018-09-25 16:27:24 -0400

    Home Page works with resizing of cards and lower screen sizes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 /* On page load */
-$(document).ready(function(){
-    //Makes every sumBox in the same row the same height
+$(document).ready(function(){ 
+    reSizeSumBoxes();
+});
+
+$(window).resize(function(){
+    reSizeSumBoxes();
+});
+
+/* UI Experience */
+//Makes every sumBox in the same row the same height
+function reSizeSumBoxes() {
+    for(let i = 1; i <= $('#sumBoxContainer > div').length; i++) {
+        $(sumBoxElement(i, 1)).css('height', ''); 
+        $(sumBoxElement(i, 2)).css('height', '');
+    }
+
     for(let i = 1; i <= $('#sumBoxContainer > div').length; i++) {
         if($(sumBoxElement(i, 1)).css('height') > $(sumBoxElement(i, 2)).css('height')) {
             $(sumBoxElement(i, 2)).css('height', $(sumBoxElement(i, 1)).css('height'));
@@ -9,8 +23,9 @@ $(document).ready(function(){
             $(sumBoxElement(i, 1)).css('height', $(sumBoxElement(i, 2)).css('height'));
         }
     }
-    
-});
+
+    //$('#content').css('min-height','calc(100% - 96px);');
+}
 
 /* Helper functions */
 //Returns the selector of the card-body of the sumBoxes for the for loop in document.ready

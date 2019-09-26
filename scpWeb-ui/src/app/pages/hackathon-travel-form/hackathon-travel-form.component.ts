e670dab12f92548f8886369f2855cde8f33ab88c
Andrew Fernandez <fdez26@ymail.com>
2019-09-26 11:36:54 -0400

    work on hackathon travel form

--- scpWeb-ui/src/app/pages/hackathon-travel-form/hackathon-travel-form.component.ts
+++ scpWeb-ui/src/app/pages/hackathon-travel-form/hackathon-travel-form.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * @title Basic Inputs
+ */
 @Component({
   selector: 'app-hackathon-travel-form',
-  templateUrl: './hackathon-travel-form.component.html',
-  styleUrls: ['./hackathon-travel-form.component.scss']
+  templateUrl: 'hackathon-travel-form.component.html',
+  styleUrls: ['hackathon-travel-form.component.scss']
 })
 export class HackathonTravelFormComponent implements OnInit {
 
